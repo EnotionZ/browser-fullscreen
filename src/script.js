a4f9e674ce42377f4cdd2b6366c24b697242f39c
Dominick Pham <dominick.pham@gmail.com>
2015-03-04 15:17:09 -0800

    fullscreen using key gestures

--- src/script.js
+++ src/script.js
@@ -1,6 +1,18 @@
 /*global chrome*/
 
-var makeFullScreen = function() {
+var getFullScreenState = function() {
+    if(typeof document.fullscreenElement !== 'undefined') {
+        return document.fullscreenElement;
+    } else if(typeof document.msFullscreenElement !== 'undefined') {
+        return document.msFullscreenElement;
+    } else if(typeof document.mozFullScreen !== 'undefined') {
+        return document.mozFullScreen;
+    } else if(typeof document.webkitIsFullScreen !== 'undefined') {
+        return document.webkitIsFullScreen;
+    }
+};
+
+var enterFullScreen = function() {
     var docElm = document.documentElement;
     if (docElm.requestFullscreen) {
         docElm.requestFullscreen();
@@ -13,18 +25,46 @@ var makeFullScreen = function() {
     }
 };
 
+var exitFullScreen = function() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+        document.webkitCancelFullScreen();
+    }
+};
+
+var toggleFullScreen = function() {
+    if(getFullScreenState()) {
+        exitFullScreen();
+    } else {
+        enterFullScreen();
+    }
+};
+
 if(typeof chrome !== 'undefined' && typeof chrome.browserAction !== 'undefined') {
     chrome.browserAction.onClicked.addListener(function() {
-        makeFullScreen();
+        enterFullScreen();
     });
 }
 
 if(typeof document !== 'undefined' && typeof document.body !== 'undefined') {
-    var b = document.createElement('button');
-    document.body.appendChild(b);
-    b.setAttribute('style', 'position: fixed; right: 0; bottom: 0;');
-    b.innerHTML = 'FullScreen';
-    b.addEventListener('click', function() {
-        makeFullScreen();
+    var KEYS = { SHIFT: 16, CTRL: 17, F: 70 };
+    var keydowns = {16: false, 17: false};
+    document.body.addEventListener('keydown', function(e) {
+        if(e.keyCode === KEYS.SHIFT || e.keyCode === KEYS.CTRL) {
+            keydowns[e.keyCode] = true;
+        }
+    });
+    document.body.addEventListener('keyup', function(e) {
+        if(e.keyCode === KEYS.SHIFT || e.keyCode === KEYS.CTRL) {
+            keydowns[e.keyCode] = false;
+        }
+        if(e.keyCode === KEYS.F && keydowns[KEYS.SHIFT] && keydowns[KEYS.CTRL]) {
+            toggleFullScreen();
+        }
     });
 }
